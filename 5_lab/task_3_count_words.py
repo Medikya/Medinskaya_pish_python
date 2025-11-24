d1d16e9ac8bbd1662a9a64185ca9154c3f673822
Ekaterina Medinskaya <yamedinskaya@mail.ru>
2025-11-24 16:26:05 +0300

    init

---
+++ 5_lab/task_3_count_words.py
@@ -0,0 +1,5 @@
+sentence = "Python is a powerful and easy-to-learn programming language."
+words = sentence.split()
+word_count = len(words)
+print("Слова:", words)
+print("Количество слов:", word_count)
